33df11582e3f99bf8eb984a9100f8b28ddaedb56
Eliot Alderson <e.alderson@allsafe.com>
2023-04-09 15:04:53 +0530

    Made the API Docs Playground and Added a Sample Command File.

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from .views import *
 
 from rest_framework import routers
@@ -7,7 +10,18 @@ router = routers.DefaultRouter()
 
 router.register('contacts', ContactsViewSet)
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Contacts API",
+      default_version='v1',
+      description="Contacts API for App Integration and Automation",
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
         path('', include(router.urls)),
-        path('/docs', index, name="API Index"),
+        path('playground', schema_view.with_ui("swagger", cache_timeout=0)),
+        path('docs', schema_view.with_ui('redoc', cache_timeout=0), name="API Index"),
     ]
---
+++ sample_cmd.sh
@@ -0,0 +1,1 @@
+curl -u 'admin:AdminPass123$' -X POST -d'name="foobar"' -d'email="foobar@ecorp.org"' -d'phone=+12345678901' 127.0.0.1:8000/api/contacts/
