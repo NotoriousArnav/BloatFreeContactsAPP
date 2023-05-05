ed435c905f7e565fa9148309f5b904a84f06c846
Eliot Alderson <e.alderson@allsafe.com>
2023-05-05 23:57:18 +0530

    Finally Peace Ig!

--- contactsapp/settings.py
+++ contactsapp/settings.py
@@ -144,8 +144,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
-STATIC_URL = '/static/'
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
@@ -154,4 +154,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
+    BASE_DIR / 'static',
 ]
