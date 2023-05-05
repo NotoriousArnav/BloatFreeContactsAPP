b089943ebf5c051179fe9c56c55a3c4977ac9dd6
Eliot Alderson <e.alderson@allsafe.com>
2023-05-05 23:06:34 +0530

    Correctifying Build Commands

--- init.sh
+++ init.sh
@@ -8,4 +8,6 @@ sleep 1
 python3 manage.py migrate
 echo "[+] Creating Admin Account"
 sleep 1
-python3 manage.py createsuperuser
+python3 manage.py shell -c "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'adminpass')"
+echo "[+] Running the server with Gunicorn"
+gunicorn contactsapp.wsgi:application
