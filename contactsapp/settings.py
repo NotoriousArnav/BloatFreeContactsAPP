c5b5fd11fa7e9a02d0c862d3adabca32572b1c09
Eliot Alderson <e.alderson@allsafe.com>
2023-05-06 00:02:02 +0530

    Finally Peace Ig!

--- contactsapp/settings.py
+++ contactsapp/settings.py
@@ -152,8 +152,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+"""
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR,"static"),
     BASE_DIR / 'static',
 ]
+"""
