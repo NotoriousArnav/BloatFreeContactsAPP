4e20382bd6765295864c4402047f2546b51c97ab
Eliot Alderson <e.alderson@allsafe.com>
2023-05-06 00:05:48 +0530

    Finally Peace Ig!

--- contactsapp/urls.py
+++ contactsapp/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
+    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
     path('/', include('contacts.urls')),
     path('', include('contacts.urls')),
     path('api/', include('api.urls')),
